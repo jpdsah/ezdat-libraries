6d2939ad0ab66c811a0725c2d673fdd01f8fed7d
Jageshwar Sah <jageshwar@ezdatechnology.com>
2022-02-24 16:52:25 +0530

    added test dependencies

--- library/core-android-ext/build.gradle.kts
+++ library/core-android-ext/build.gradle.kts
@@ -48,6 +48,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
 
 
